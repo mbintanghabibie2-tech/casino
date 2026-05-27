9a89a003e5e3fb1c39ed3f2441dfacb4bed603ae
mbintanghabibie2-tech <mbintanghabibie2@gmail.com>
2026-05-27 20:49:46 +0700

    supabase.js

---
+++ database/supabase.js
@@ -0,0 +1,14 @@
+const { createClient } =
+require('@supabase/supabase-js')
+
+const supabase =
+createClient(
+
+'https://eextshaeqrhaeevlosex.supabase.co',
+
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVleHRzaGFlcXJoYWVldmxvc2V4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NjM4MDQsImV4cCI6MjA5NTQzOTgwNH0.poPZA0dqC5lpEU2EgLE2F4OTvqoZNcHUv-2hpZ0vAxI'
+
+)
+
+module.exports =
+supabase
